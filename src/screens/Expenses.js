b758207673af70eec239c861b7a0a820455b77f6
Tanmoy Sarker <tanmoy@Tanmoys-Air.localdomain>
2021-03-23 16:24:27 +0600

    filter tab design added

--- src/screens/Expenses.js
+++ src/screens/Expenses.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { View, Text, TouchableOpacity, Button, Dimensions } from 'react-native';
+import styles from '../assests/styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default () => (
-  <View style={{marginTop: 50}}>
-    <Text>Expenses</Text>
-  </View>
-);
+export default Expense = () => {
+    return(
+    <View style={styles.toolbarContainer}>
+    <TouchableOpacity style={styles.toolbarLeft}>
+        <Icon
+            style={styles.toolbarIcon}
+            name='angle-down'
+            size={18}
+        />
+        <Text style={{ color: '#818995', marginLeft: 5 }}>Categories</Text>
+    </TouchableOpacity>
+    <View
+        style={[
+            styles.toolbarRight
+        ]}>
+            <TouchableOpacity
+                style={{ flexDirection: 'row' }}>
+                <Text style={{ color: '#818995', marginRight: 5 }}>
+                    Week
+                </Text>
+                <Icon
+                    style={styles.toolbarIcon}
+                    name='angle-down'
+                    size={18}
+                />
+            </TouchableOpacity>
+        <TouchableOpacity
+            style={{ flexDirection: 'row' }}>
+            <Text style={{ color: '#818995', marginRight: 5 }}>
+                 Month
+            </Text>
+            <Icon
+                style={styles.toolbarIcon}
+                name='angle-down'
+                size={18}
+            />
+        </TouchableOpacity>
+    </View>
+    </View>
+    )
+}
